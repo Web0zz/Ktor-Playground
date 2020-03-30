076a293878b26b45393081083dc337d39ac17823
hadihariri <mail@hadihariri.com>
2020-03-30 18:55:20 +0200

    Initial push

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,6 +1,13 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
+import freemarker.cache.*
+import io.ktor.application.*
+import io.ktor.freemarker.*
+import io.ktor.html.respondHtml
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import kotlinx.html.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)

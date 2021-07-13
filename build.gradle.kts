bcd3003f9f2ebecd454e55ebe60470108301453e
Web0zz <furkan.ozen4@ogr.sakarya.edu.tr>
2021-07-14 02:00:30 +0300

    Learning http api setup and testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.5.21"
+    kotlin("plugin.serialization") version "1.5.21"
 }
 
 group = "com.web0zz"
@@ -21,6 +22,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-serialization:$ktor_version")
 
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
--- src/main/kotlin/com/web0zz/Application.kt
+++ src/main/kotlin/com/web0zz/Application.kt
@@ -1,10 +1,18 @@
 package com.web0zz
 
-import io.ktor.application.Application
+import com.web0zz.routing.registerOrderRoutes
+import com.web0zz.routing.registerUserRoutes
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.serialization.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-fun Application.module() {
-
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        json()
+    }
+    registerUserRoutes()
+    registerOrderRoutes()
 }

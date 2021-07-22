60a6d0a4b7cef2e1d14461e8ffa59b7bf2a35c76
Web0zz <furkan.ozen4@ogr.sakarya.edu.tr>
2021-07-22 23:45:22 +0300

    setup jwt

--- src/main/kotlin/com/web0zz/routing/AuthRouting.kt
+++ src/main/kotlin/com/web0zz/routing/AuthRouting.kt
@@ -1,63 +1,22 @@
 package com.web0zz.routing
 
-import com.web0zz.auth.JwtConfig
-import com.web0zz.model.User
 import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.request.*
-import io.ktor.response.*
 import io.ktor.routing.*
-import org.kodein.db.DB
-import org.kodein.db.find
-import org.kodein.db.getById
 
-fun Application.registerAuthRoutes(db: DB) {
+fun Application.registerAuthRoutes() {
     routing {
-        authUser(db)
+        authUser()
     }
 }
 
-fun Route.authUser(db: DB) {
+fun Route.authUser() {
     route("/auth") {
         post("/register") {
-            val jwt = JwtConfig.instance
 
-            val user = runCatching { call.receive<User>() }.getOrElse {
-                return@post call.respondText(
-                    "Missing or malformed user",
-                    status = HttpStatusCode.BadRequest
-                )
-            }
-
-            if(db.getById<User>(user.id) != null) {
-                return@post call.respondText(
-                    "User is not available",
-                    status = HttpStatusCode.BadRequest
-                )
-            }
-
-            db.put(user)
-            call.respond(jwt.sign(user.id))
         }
 
         post("/login") {
-            val jwt = JwtConfig.instance
 
-            val user = runCatching { call.receive<User>() }.getOrElse {
-                return@post call.respondText(
-                    "Missing or malformed user",
-                    status = HttpStatusCode.BadRequest
-                )
-            }
-
-            if(db.getById<User>(user.id) == null) {
-                return@post call.respondText(
-                    "User is not available",
-                    status = HttpStatusCode.BadRequest
-                )
-            }
-
-            call.respond(jwt.sign(user.id))
         }
     }
 }

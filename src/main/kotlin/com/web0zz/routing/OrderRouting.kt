29f416244544eba517a372593acaa33e59d41979
Web0zz <furkan.ozen4@ogr.sakarya.edu.tr>
2021-07-23 20:43:58 +0300

    Jwt ready

--- src/main/kotlin/com/web0zz/routing/OrderRouting.kt
+++ src/main/kotlin/com/web0zz/routing/OrderRouting.kt
@@ -2,45 +2,54 @@ package com.web0zz.routing
 
 import com.web0zz.model.orderStorage
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.registerOrderRoutes() {
     routing {
-        listOrdersRoute()
-        getOrderRoute()
-        totalizeOrderRoute()
+        orderRoute()
     }
 }
 
-fun Route.listOrdersRoute() {
-    get("/order") {
-        if (orderStorage.isNotEmpty()) {
-            call.respond(orderStorage)
+fun Route.orderRoute() {
+    authenticate {
+        get("/order") {
+            call.principal<UserIdPrincipal>()
+                ?: return@get call.respond("Access Denied")
+
+            if (orderStorage.isNotEmpty()) {
+                call.respond(orderStorage)
+            } else call.respond("Not Found Any Data")
         }
-    }
-}
 
-fun Route.getOrderRoute() {
-    get("/order/{id}") {
-        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
-        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
-            "Not Found",
-            status = HttpStatusCode.NotFound
-        )
-        call.respond(order)
-    }
-}
+        route("/order/") {
 
-fun Route.totalizeOrderRoute() {
-    get("/order/{id}/total") {
-        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
-        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
-            "Not Found",
-            status = HttpStatusCode.NotFound
-        )
-        val total = order.contents.map { it.price * it.amount }.sum()
-        call.respond(total)
+            get("{id}") {
+               call.principal<UserIdPrincipal>()
+                    ?: return@get call.respond("Access Denied")
+
+                val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+                val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+                    "Not Found",
+                    status = HttpStatusCode.NotFound
+                )
+                call.respond(order)
+            }
+
+            get("{id}/total") {
+                call.principal<UserIdPrincipal>()
+                    ?: return@get call.respond("Access Denied")
+
+                val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+                val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+                    "Not Found",
+                    status = HttpStatusCode.NotFound
+                )
+                val total = order.contents.map { it.price * it.amount }.sum()
+                call.respond(total)
+            }
+        }
     }
 }
